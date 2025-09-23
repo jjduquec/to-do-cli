80e5a441829cd0a3e4308687d5252c49c94b3e3d
Juan Duque <juanjo.dc@outlook.com>
2025-09-23 07:58:34 -0500

    parameters implemented

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("first print of to do proyect")
+import argparse 
+
+parse= argparse.ArgumentParser(prog="TO DO cli") 
+parse.add_argument("-task","-t",help="name of the task to be added ")
+parse.add_argument("-list","-l",help="name of the task list to be created or to add a task")
+
+
+
+if __name__=='__main__': 
+    #getting the parameters  
+    args=parse.parse_args() 
+    if args.task and  args.list:  
+        print("parameters received")
+    elif args.task:  
+        #task is added to a default list  
+        print("task will be added to default list")
+    elif args.list: 
+        print("list will be created")
+    else:  
+        print("execute interface")
